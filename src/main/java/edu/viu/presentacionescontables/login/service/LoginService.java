eac35d5381a5a457b6aba3038b3f3f3ac74b9457
isramoli <israelmolino@gmail.com>
2022-01-13 08:26:13 +0100

    quitada anotación sobrante

--- src/main/java/edu/viu/presentacionescontables/login/service/LoginService.java
+++ src/main/java/edu/viu/presentacionescontables/login/service/LoginService.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service("jpaUserDetailsService")
+@Service
 public class LoginService implements UserDetailsService {
 
     private final Logger logger = LoggerFactory.getLogger(LoginService.class);
